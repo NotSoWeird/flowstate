c5cc8ea15153f34034dc605a5b89c78853e919fd
NotSoWeird <wiktor@liew.se>
2023-12-29 16:47:36 +0100

    forgot to change option to gnome

--- modules/nixos/desktop/gnome/default.nix
+++ modules/nixos/desktop/gnome/default.nix
@@ -1,9 +1,9 @@
 { options, config, lib, pkgs, ... }:
 with lib;
 with lib.flowstate;
-let cfg = config.flowstate.system.xkb;
+let cfg = config.flowstate.desktop.gnome;
 in {
-  options.flowstate.system.xkb = with types; {
+  options.flowstate.desktop.gnome = with types; {
     enable = mkBoolOpt false "Whether or not to enable Gnome";
   };
 
