ce9f2f1554ef839118eadb346c2c4a4bf0b8932e
NotSoWeird <wiktor@liew.se>
2024-03-06 09:45:41 +0100

    added monitor disabling when lid closes

--- modules/nixos/apps/editor/doom-emacs/default.nix
+++ modules/nixos/apps/editor/doom-emacs/default.nix
@@ -30,7 +30,7 @@ in {
             emacsql-sqlite
             pdf-tools
             vterm
-            treesit-grammars.with-all-grammars
+            treesit-grammars.with-all-grammarr
           ];
       };
     };
--- modules/nixos/desktop/hyprland/default.nix
+++ modules/nixos/desktop/hyprland/default.nix
@@ -88,6 +88,12 @@ in {
               monitor=,preferred,auto,1 #For random monitors
               #monitor = ,preferred,auto,auto
 
+              # trigger when the switch is turning on
+              bindl=,switch:on:Lid Switch,exec,hyprctl keyword monitor "eDP-1, 3840x2160@60, 0x0, 2"
+              # trigger when the switch is turning off
+              bindl=,switch:off:Lid Switch,exec,hyprctl keyword monitor "eDP-1, disable"
+
+
               # Fix slow startup
               #exec systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
               #exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
